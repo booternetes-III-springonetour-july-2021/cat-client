e187ba3b19c0cc5bbc9ca2fbc172ecbb12274a0b
Josh Long <josh@joshlong.com>
2021-06-23 00:41:49 -0700

    no-contract

--- src/test/java/com/example/catclient/CatClientApplicationTests.java
+++ src/test/java/com/example/catclient/CatClientApplicationTests.java
@@ -1,24 +1,9 @@
 package com.example.catclient;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.support.TestPropertySourceUtils;
-
-@AutoConfigureStubRunner(
-	ids = "booternetes:cat-service:+:8080",
-	stubsMode = StubRunnerProperties.StubsMode.LOCAL
-)
-//@AutoConfigureWireMock (port = 8080)
 @SpringBootTest
 class CatClientApplicationTests {
 
