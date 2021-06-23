5788d7f52a6a673f773bf5ae3c9c8e73df53b492
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2021-06-23 09:45:48 +0200

    Add stubrunner + wiremock.

--- src/test/java/com/example/catclient/CatClientApplicationTests.java
+++ src/test/java/com/example/catclient/CatClientApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.catclient;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
 @SpringBootTest
 class CatClientApplicationTests {
 
@@ -14,17 +18,15 @@ class CatClientApplicationTests {
 	@Test
 	void contextLoads() {
 
-		/*
 			WireMock
 			.stubFor(WireMock
-				.get("/cats/Toby")
+				.get("/cat/Toby")
 				.willReturn(
 					WireMock
 						.aResponse()
 						.withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
 						.withBody("  { \"id\":\"1\", \"name\":\"Toby\" }  ")
 				));
-				*/
 		var result = this.client.getCatByName("Toby");
 		Assertions.assertEquals(result.getName(), "Toby");
 
