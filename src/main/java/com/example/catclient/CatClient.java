a2844cee0ce8913b2d8684216098a514ecd02393
Mario Gray <mario.gray@gmail.com>
2021-07-12 12:08:15 -0700

    req for B-4 content

--- src/main/java/com/example/catclient/CatClient.java
+++ src/main/java/com/example/catclient/CatClient.java
@@ -10,28 +10,28 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class CatClient {
 
-	private final RestTemplate restTemplate;
-	private final String hostname;
-	private final int port;
+    private final RestTemplate restTemplate;
+    private final String hostname;
+    private final int port;
 
-	public CatClient(RestTemplate restTemplate,
-																		@Value("${cats.host}") String hostname,
-																		@Value("${cats.port}") int port) {
-		this.restTemplate = restTemplate;
-		this.hostname = hostname;
-		this.port = port;
-	}
+    public CatClient(RestTemplate restTemplate,
+                     @Value("${cats.host}") String hostname,
+                     @Value("${cats.port}") int port) {
+        this.restTemplate = restTemplate;
+        this.hostname = hostname;
+        this.port = port;
+    }
 
-	Cat getCatByName(String name) {
-		return this.restTemplate.getForObject("http://" + this.hostname +
-			":" + this.port + "/cats/{name}", Cat.class, name);
-	}
+    Cat getCatByName(String name) {
+        return this.restTemplate.getForObject("http://" + this.hostname +
+                ":" + this.port + "/cats/{name}", Cat.class, name);
+    }
 }
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 class Cat {
-	private Integer id;
-	private String name;
+    private Integer id;
+    private String name;
 }
--- src/test/java/com/example/catclient/CatClientApplicationTests.java
+++ src/test/java/com/example/catclient/CatClientApplicationTests.java
@@ -1,48 +1,26 @@
 package com.example.catclient;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.support.TestPropertySourceUtils;
 
 @AutoConfigureStubRunner(
-	ids = "booternetes:cat-service:+:8080",
-	stubsMode = StubRunnerProperties.StubsMode.LOCAL
+        ids = "booternetes:cat-service:+:8080",
+        stubsMode = StubRunnerProperties.StubsMode.LOCAL
 )
-//@AutoConfigureWireMock (port = 8080)
 @SpringBootTest
 class CatClientApplicationTests {
 
-	@Autowired
-	private CatClient client;
+    @Autowired
+    private CatClient client;
 
+    @Test
+    void contextLoads() {
+        var result = this.client.getCatByName("Toby");
+        Assertions.assertEquals(result.getName(), "Toby");
 
-	@Test
-	void contextLoads() {
-
-		/*
-			WireMock
-			.stubFor(WireMock
-				.get("/cats/Toby")
-				.willReturn(
-					WireMock
-						.aResponse()
-						.withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-						.withBody("  { \"id\":\"1\", \"name\":\"Toby\" }  ")
-				));
-				*/
-		var result = this.client.getCatByName("Toby");
-		Assertions.assertEquals(result.getName(), "Toby");
-
-	}
-
+    }
 }
